c64064a69ed7627c473e81e1fc76183c0fec742f
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-01-12 15:44:05 -0300

    commit before entityframework

--- EstablishmentManagerAPI/Program.cs
+++ EstablishmentManagerAPI/Program.cs
@@ -1,7 +1,3 @@
-using EstablishmentManagerLibrary.ClientRelated;
-using EstablishmentManagerLibrary.Database;
-using EstablishmentManagerLibrary.Database.CRUD;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -12,11 +8,6 @@ app.MapGet("/", () =>
 
 app.MapGet("/database", () =>
 {
-    Database_setup.CreatDataBase();
-    Client client = new Client("NomeTeste", "14758748954", DateTime.Now, "147852354", 0, 50);
-    var myclient = Select.Client(client);
-    Client_address client_address = new Client_address();
-    
     return "Done!";
 });
 

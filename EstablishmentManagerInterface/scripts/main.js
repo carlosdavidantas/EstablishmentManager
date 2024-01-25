3caf3f6bdf7f2477869f2ed5b8e0eaffe480fff9
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-01-25 19:50:45 -0300

    add preload script

--- EstablishmentManagerInterface/scripts/main.js
+++ EstablishmentManagerInterface/scripts/main.js
@@ -1,17 +1,22 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow} = require("electron");
+const nodePath = require("node:path");
 const openAPI = require("./openAPI.js");
 
 const createWindow = () => {
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: nodePath.join(__dirname, "preload.js")
+        }
     });
+    
     window.loadFile('pages\\login.html');
-    openAPI.execute();
 }
 
 app.whenReady().then(() => {
     createWindow();
+    openAPI.execute();
 });
 
 app.on('window-all-closed', () => {

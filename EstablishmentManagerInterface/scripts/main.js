9743fd3ccdc9d3665a5c6aa117f0afde080d54ef
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-01-26 20:26:14 -0300

    some refactorings

--- EstablishmentManagerInterface/scripts/main.js
+++ EstablishmentManagerInterface/scripts/main.js
@@ -11,7 +11,7 @@ const createWindow = () => {
         }
     });
     
-    window.loadFile('pages\\login.html');
+    window.loadFile("./pages/login/login.html");
 }
 
 app.whenReady().then(() => {

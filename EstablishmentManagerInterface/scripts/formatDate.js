c5ad0240d82927d5918fde534a5098a65a4871da
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-05-23 19:13:16 -0300

    create formatDate function

---
+++ EstablishmentManagerInterface/scripts/formatDate.js
@@ -0,0 +1,8 @@
+function execute(date) {
+    const day = date.slice(8, 10);
+    const month = date.slice(5, 7);
+    const year = date.slice(0, 4);
+    return `${day}/${month}/${year}`
+}
+
+module.exports = { execute };

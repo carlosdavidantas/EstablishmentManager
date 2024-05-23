6b2bd05853ea81aa55c856c3d6b69ce2db7b259b
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-05-23 17:42:23 -0300

    added data insert verification

--- EstablishmentManagerInterface/pages/client/createClient/createClient.js
+++ EstablishmentManagerInterface/pages/client/createClient/createClient.js
@@ -213,3 +213,35 @@ clientSaveButton.addEventListener("click", async () => {
         }
     }
 });
+
+
+birthdayTextBoxElement.addEventListener("input", function(e) {  // Function that verificates data sent by user.
+    let value = e.target.value.replace(/\D/g, '');
+    
+    if(value.length >= 2) {
+        let day = parseInt(value.slice(0,2), 10);
+        if(day > 31){
+            value = "31";
+        }else {
+            value = value.slice(0,2) + "/" + value.slice(2);
+        }
+    }
+
+    if(value.length >= 5){
+        let month = parseInt(value.slice(3, 5), 10);
+        if (month > 12) {
+            value = value.slice(0, 2) + "/12";
+        } else {
+            value = value.slice(0, 5) + "/" + value.slice(5);
+        }
+    }
+    if(value.length >= 10){
+        let year = parseInt(value.slice(6,10));
+        let actualYear = new Date().getFullYear();
+        if(year > actualYear){
+            value = value.slice(0, 6) + `${actualYear}`;
+        }
+    }
+    
+    e.target.value = value;
+});

81592529fd1ee9b6f3351c791c1588b068e8a2a2
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-07-30 18:23:10 -0300

    added listener to open tables page

--- EstablishmentManagerInterface/pages/home/home.js
+++ EstablishmentManagerInterface/pages/home/home.js
@@ -3,3 +3,7 @@ const { ipcRenderer } = require('electron');
 document.getElementById("Clients").addEventListener("click", () => {
     ipcRenderer.send("openAllClients");
 })
+
+document.getElementById("Tables").addEventListener("click", () => {
+    ipcRenderer.send("openTables");
+})
